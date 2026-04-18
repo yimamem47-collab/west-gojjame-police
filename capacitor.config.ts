c4b1af164c70baa1bb81780ac8e9bd4102f46e03
yimamem47-collab <yimamem47@gmail.com>
2026-04-18 03:51:30 -0800

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.westgojjam.police',
   appName: 'West Gojjam Police',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    StatusBar: {
+      style: 'DARK',
+      backgroundColor: '#1A237E',
+      overlaysWebView: false
+    }
+  }
 };
 
 export default config;
